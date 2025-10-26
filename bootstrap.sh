ca6d4d9fead40a35ad380eaa32bf027189284bac
andrewwkimm <andrewkimka@gmail.com>
2025-10-26 21:08:20 +0000

    Change to the actual project name

--- bootstrap.sh
+++ bootstrap.sh
@@ -24,11 +24,15 @@ sed -i "s/--cov=project_name/--cov=${project_name}/" .github/workflows/ci.yaml
 # Rename project directory
 mv project_name "${project_name}"
 
+# Update project __init__.py
+sed -i "s/\"The Python project package.\"/\"The ${project_name} package.\"/" "${project_name}/__init__.py"
+
+# Update tests __init__.py
+sed -i "s/\"Test for the Python application.\"/\"Tests for ${project_name}.\"/" "tests/__init__.py"
+
 # Rename and update test file
 mv tests/test_project_name.py "tests/test_${project_name}.py"
 sed -i "s/from project_name/from ${project_name}/" "tests/test_${project_name}.py"
+sed -i "s/\"Tests for project version.\"/\"Tests for ${project_name} version.\"/" "tests/test_${project_name}.py"
 
 echo "Setup complete!"
-
-# Remove setup script
-rm -- "$0"
